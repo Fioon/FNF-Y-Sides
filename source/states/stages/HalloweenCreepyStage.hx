2b14756801cf2a1ec6046e35b6cb01ffecc8b380
MrMadera <ismaelglhs@gmail.com>
2025-12-19 20:35:09 +0000

    no to shaders for a moment,,

--- source/states/stages/HalloweenCreepyStage.hx
+++ source/states/stages/HalloweenCreepyStage.hx
@@ -34,84 +34,87 @@ class HalloweenCreepyStage extends BaseStage
 		gradient.blend = ADD;
 		add(gradient);
 
-		var bloom = new BloomShader();
-
-		bloom.dim.value = [1.8]; // 1.8
-		bloom.Directions.value = [4.0]; // 2.0, 100.0 to remove
-		bloom.Quality.value = [8.0]; // 8.0
-		bloom.Size.value = [4.0]; // 8.0, 1.0
-
-		var shaderFilter = new ShaderFilter(bloom);
-		FlxG.camera.filters = [shaderFilter];
-
-		chromaticAberration = new ChromaticAberration();
-		chromaticAberration.rOffset.value = [0.001];
-		chromaticAberration.gOffset.value = [0.0];
-		chromaticAberration.bOffset.value = [-0.001];
-
-		var shaderFilter2 = new ShaderFilter(chromaticAberration);
-		FlxG.camera.filters.push(shaderFilter2);
-
-		// lights on characters
-		var rimBF = new DropShadowShader();
-		rimBF.setAdjustColor(-46, -38, -25, -20);
-		rimBF.color = 0xFFE9001F;
-		game.boyfriend.shader = rimBF;
-		rimBF.attachedSprite = game.boyfriend;
-
-		game.boyfriend.animation.callback = function()
+		if(ClientPrefs.data.shaders)
 		{
-			if (game.boyfriend != null)
+			var bloom = new BloomShader();
+
+			bloom.dim.value = [1.8]; // 1.8
+			bloom.Directions.value = [4.0]; // 2.0, 100.0 to remove
+			bloom.Quality.value = [8.0]; // 8.0
+			bloom.Size.value = [4.0]; // 8.0, 1.0
+
+			var shaderFilter = new ShaderFilter(bloom);
+			FlxG.camera.filters = [shaderFilter];
+
+			chromaticAberration = new ChromaticAberration();
+			chromaticAberration.rOffset.value = [0.001];
+			chromaticAberration.gOffset.value = [0.0];
+			chromaticAberration.bOffset.value = [-0.001];
+
+			var shaderFilter2 = new ShaderFilter(chromaticAberration);
+			FlxG.camera.filters.push(shaderFilter2);
+
+			// lights on characters
+			var rimBF = new DropShadowShader();
+			rimBF.setAdjustColor(-46, -38, -25, -20);
+			rimBF.color = 0xFFE9001F;
+			game.boyfriend.shader = rimBF;
+			rimBF.attachedSprite = game.boyfriend;
+
+			game.boyfriend.animation.callback = function()
 			{
-				rimBF.updateFrameInfo(game.boyfriend.frame);
-			}
-		};
-
-		var rimGF = new DropShadowShader();
-		rimGF.setAdjustColor(-46, -38, -25, -20);
-		rimGF.color = 0xFFE9001F;
-		game.gf.shader = rimGF;
-		rimGF.attachedSprite = game.gf;
-		rimGF.distance = 10;
-
-		game.gf.animation.callback = function()
-		{
-			if (game.gf != null)
-			{
-				rimGF.updateFrameInfo(game.gf.frame);
-			}
-		};
-
-		var rimDad = new DropShadowShader();
-		rimDad.setAdjustColor(-46, -38, -25, -20);
-		rimDad.color = 0xFFE9001F;
-		game.dad.shader = rimDad;
-		rimDad.attachedSprite = game.dad;
-		rimDad.angle = 180;
-
-		game.dad.animation.callback = function()
-		{
-			if (game.dad != null)
-			{
-				rimDad.updateFrameInfo(game.dad.frame);
-			}
-		};
-
-		if (game.player3 != null)
-		{
-			var rimPlayer3 = new DropShadowShader();
-			rimPlayer3.setAdjustColor(-46, -38, -25, -20);
-			rimPlayer3.color = 0xFFE9001F;
-			game.player3.shader = rimPlayer3;
-			rimPlayer3.attachedSprite = game.player3;
-			rimPlayer3.angle = 180;
-			game.player3.animation.callback = function()
-			{
-				if (game.player3 != null)
+				if (game.boyfriend != null)
 				{
-					rimPlayer3.updateFrameInfo(game.player3.frame);
+					rimBF.updateFrameInfo(game.boyfriend.frame);
 				}
 			};
+
+			var rimGF = new DropShadowShader();
+			rimGF.setAdjustColor(-46, -38, -25, -20);
+			rimGF.color = 0xFFE9001F;
+			game.gf.shader = rimGF;
+			rimGF.attachedSprite = game.gf;
+			rimGF.distance = 10;
+
+			game.gf.animation.callback = function()
+			{
+				if (game.gf != null)
+				{
+					rimGF.updateFrameInfo(game.gf.frame);
+				}
+			};
+
+			var rimDad = new DropShadowShader();
+			rimDad.setAdjustColor(-46, -38, -25, -20);
+			rimDad.color = 0xFFE9001F;
+			game.dad.shader = rimDad;
+			rimDad.attachedSprite = game.dad;
+			rimDad.angle = 180;
+
+			game.dad.animation.callback = function()
+			{
+				if (game.dad != null)
+				{
+					rimDad.updateFrameInfo(game.dad.frame);
+				}
+			};
+
+			if (game.player3 != null)
+			{
+				var rimPlayer3 = new DropShadowShader();
+				rimPlayer3.setAdjustColor(-46, -38, -25, -20);
+				rimPlayer3.color = 0xFFE9001F;
+				game.player3.shader = rimPlayer3;
+				rimPlayer3.attachedSprite = game.player3;
+				rimPlayer3.angle = 180;
+				game.player3.animation.callback = function()
+				{
+					if (game.player3 != null)
+					{
+						rimPlayer3.updateFrameInfo(game.player3.frame);
+					}
+				};
+			}
 		}
 	}
 
@@ -119,9 +122,12 @@ class HalloweenCreepyStage extends BaseStage
 	{
 		if(curStep == 1088)
 		{
-			chromaticAberration.rOffset.value = [0.002];
-			chromaticAberration.gOffset.value = [0.0];
-			chromaticAberration.bOffset.value = [-0.002];
+			if(ClientPrefs.data.shaders)
+			{
+				chromaticAberration.rOffset.value = [0.002];
+				chromaticAberration.gOffset.value = [0.0];
+				chromaticAberration.bOffset.value = [-0.002];
+			}
 		}
 	}
 }
